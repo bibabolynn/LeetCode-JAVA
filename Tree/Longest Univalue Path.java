28e76c0fbd4a80d9fa32f1b0b5811fb270fe45d8
bibabolynn <1018527906@qq.com>
2018-05-15 07:57:26 +0800

    Create Longest Univalue Path.java

---
+++ Tree/Longest Univalue Path.java
@@ -0,0 +1,78 @@
+Problem: 
+Given a binary tree, find the length of the longest path where each node in the path has the same value. This path may or may not pass through the root.
+
+Note: The length of path between two nodes is represented by the number of edges between them.
+
+Example 1:
+
+Input:
+
+              5
+             / \
+            4   5
+           / \   \
+          1   1   5
+Output:
+
+2
+Example 2:
+
+Input:
+
+              1
+             / \
+            4   5
+           / \   \
+          4   4   5
+Output:
+
+2
+Note: The given binary tree has not more than 10000 nodes. The height of the tree is not more than 1000.
+
+
+Thought: 比较的时候要比较最大长度和arrowLeft+arrowRight，但是返回要返回arrowLeft和arrowRight中的最大值，因为，左子树和右子树都只能是一条线，
+而不能是同时延伸到左右子树的一条线
+不然就会出现                返回是3，而不是2
+              4
+             / \
+            4   5
+           / \   \
+          4   4   5
+
+Complexity Analysis
+
+Time Complexity: O(N), where NN is the number of nodes in the tree. We process every node once.
+
+Space Complexity: O(H), where H is the height of the tree. Our recursive call stack could be up to HH layers deep.
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+ class Solution {
+    int ans;
+    public int longestUnivaluePath(TreeNode root) {
+        ans = 0;
+        arrowLength(root);
+        return ans;
+    }
+    public int arrowLength(TreeNode node) {
+        if (node == null) return 0;
+        int left = arrowLength(node.left);
+        int right = arrowLength(node.right);
+        int arrowLeft = 0, arrowRight = 0;
+        if (node.left != null && node.left.val == node.val) {
+            arrowLeft += left + 1;
+        }
+        if (node.right != null && node.right.val == node.val) {
+            arrowRight += right + 1;
+        }
+        ans = Math.max(ans, arrowLeft + arrowRight);
+        return Math.max(arrowLeft, arrowRight);
+    }
+}
