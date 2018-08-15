111bf74c4def4eab2e17b513091159f86d77d33c
bibabolynn <1018527906@qq.com>
2018-08-15 11:46:08 +0800

    Create  Predict the Winner.java

---
+++ Dynamic Programming/Predict the Winner.java
@@ -0,0 +1,74 @@
+Problem : 
+Given an array of scores that are non-negative integers. Player 1 picks one of the numbers from either end of the array followed by the player 2 and then player 1 and so on. Each time a player picks a number, that number will not be available for the next player. This continues until all the scores have been chosen. The player with the maximum score wins.
+
+Given an array of scores, predict whether player 1 is the winner. You can assume each player plays to maximize his score.
+
+Example 1:
+Input: [1, 5, 2]
+Output: False
+Explanation: Initially, player 1 can choose between 1 and 2. 
+If he chooses 2 (or 1), then player 2 can choose from 1 (or 2) and 5. If player 2 chooses 5, then player 1 will be left with 1 (or 2). 
+So, final score of player 1 is 1 + 2 = 3, and player 2 is 5. 
+Hence, player 1 will never be the winner and you need to return False.
+Example 2:
+Input: [1, 5, 233, 7]
+Output: True
+Explanation: Player 1 first chooses 1. Then player 2 have to choose between 5 and 7. No matter which number player 2 choose, player 1 can choose 233.
+Finally, player 1 has more score (234) than player 2 (12), so you need to return True representing player1 can win.
+Note:
+1 <= length of the array <= 20.
+Any scores in the given array are non-negative integers and will not exceed 10,000,000.
+If the scores of both players are equal, then player 1 is still the winner.
+
+Thought : scorePlayer_1≥scorePlayer_22 . Or in other terms, scorePlayer_1−scorePlayer_2≥0.  递归，turn 1  player 1  turn -1 player 2
+Complexity Analysis :
+Time Complexity: O(2^n),n is the length of nums
+Space Complexity: O(n)
+
+public class Solution {
+    public boolean PredictTheWinner(int[] nums) {
+        return winner(nums, 0, nums.length - 1, 1) >= 0;
+    }
+    public int winner(int[] nums, int s, int e, int turn) {
+        if (s == e)
+            return turn * nums[s];
+        int a = turn * nums[s] + winner(nums, s + 1, e, -turn);
+        int b = turn * nums[e] + winner(nums, s, e - 1, -turn);
+        return turn * Math.max(turn * a, turn * b);
+    }
+}
+
+Thought:max(nums[x]−score[x+1,y] ,nums[y]−score[x,y−1])     dp[i,j]=nums[i]−dp[i+1][j],nums[j]−dp[i][j−1].  只用到本行前一列的和本列下一行的，把本列下一行的，放到本列本行，就用到本行的前一列和本列，然后再更新本列，更新本列的时候本列旧的数据已经用过了，以后不会再用了
+Time Complexity: O(n^2),n is the length of nums
+Space Complexity: O(n)
+
+public class Solution {
+    public boolean PredictTheWinner(int[] nums) {
+        int[] dp = new int[nums.length];
+        for (int s = nums.length; s >= 0; s--) {
+            for (int e = s + 1; e < nums.length; e++) {
+                int a = nums[s] - dp[e];
+                int b = nums[e] - dp[e - 1];
+                dp[e] = Math.max(a, b);
+            }
+        }
+        return dp[nums.length - 1] >= 0;
+    }
+}
+
+二维的
+public class Solution {
+    public boolean PredictTheWinner(int[] nums) {
+        int[][] dp = new int[nums.length + 1][nums.length];
+        for (int s = nums.length; s >= 0; s--) {
+            for (int e = s + 1; e < nums.length; e++) {
+                int a = nums[s] - dp[s + 1][e];
+                int b = nums[e] - dp[s][e - 1];
+                dp[s][e] = Math.max(a, b);
+            }
+        }
+        return dp[0][nums.length - 1] >= 0;
+    }
+}
+
+
